9639eecb27d51acbe74e2d36e25e0dfc9120426b
Aerozw <aerozw@yeah.net>
2023-04-01 23:36:07 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,53 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="Box1">
+    <h1>这是一个页面测试</h1>
+  </div>
+  <div id="Box2">
+    <div class="main">
+      <div>
+        <h3 class="Name">App组件:</h3>
+        这是App组件
+      </div>
+      <div>
+        <h3 class="Name">组件测试:</h3>
+        <MyComponent></MyComponent>
+      </div>
     </div>
-  </header>
 
-  <RouterView />
+    <div class="RouterList">
+      <h3 class="Name">路由测试:</h3>
+      <div>
+        <router-link to="/">Home</router-link>
+      </div>  
+      <div>
+        <router-link to="/Page1">Page1</router-link>
+      </div>
+    </div>
+
+    <router-view></router-view>
+  </div>
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+#Box1{
+  margin-top: 50px;
+  color: green;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#Box2{
+  margin-left: 30px;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.main{
+  margin-top: 20px;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.RouterList{
+  margin-top: 20px;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.Name{
+  color: blue;
 }
 </style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    这是MyComponent组件
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
