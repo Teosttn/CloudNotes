75b0aa2734da466a80943f2d8f17ec33862d9456
Aerozw <aerozw@yeah.net>
2023-04-11 22:34:02 +0800

    zw commit to master 5

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 const Home =()=> import('../views/Home/Home.vue')
 const Register =()=> import('../views/Home/Register/RegisterPage.vue')
 const Main =()=> import('../views/Main/MainPage.vue')
-const Test =()=> import('../views/Main/Test/Test.vue')
 const AddNote =()=> import('../views/Main/AddNote/AddNote.vue')
 const EditNote =()=> import('../views/Main/EditNote/EditNote.vue')
 
@@ -22,11 +21,6 @@ const routes= [
     path:'/Main',
     name:Main,
     component:Main
-  },
-  {
-    path:'/Main/Test',
-    name:Test,
-    component:Test
   },
   {
     path:'/Main/AddNote',
